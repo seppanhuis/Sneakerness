576f966eb07f81355a640a198a85db55f4fb62bb
ozer <337706@student.mboutrecht.nl>
2025-09-19 10:43:18 +0200

    Stand Controller gemaakt

---
+++ app/views/Stands/index.php
@@ -0,0 +1,4 @@
+<?php require_once APPROOT . '/views/includes/header.php'; ?>
+<H1>wad</H1>
+
+<?php require_once APPROOT . '/views/includes/footer.php'; ?>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>MVC OOP PDO</title>
+  <title>Sneakerness</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="<?= URLROOT; ?>/public/css/style.css">
@@ -19,7 +19,7 @@
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="<?= URLROOT; ?>/homepages/index" class="nav-link px-2 text-white">Home</a></li>
           <li><a href="<?= URLROOT; ?>/ticket/index" class="nav-link px-2 text-white">Ticket</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="<?= URLROOT; ?>/Stands/index" class="nav-link px-2 text-white">Stands</a></li>
           <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
           <li><a href="#" class="nav-link px-2 text-white">About</a></li>
           <li><a href="<?= URLROOT; ?>/ContactPersoon/index" class="nav-link px-2 text-white">Contactpersonen</a></li>
